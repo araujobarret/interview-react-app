ff5f4257071530914582044907df4651f2a1ce94
araujobarret <araujobarret@gmail.com>
2020-02-25 16:33:04 -0300

    Adds main diagonal and anti diagonal validation

--- src/GameService.js
+++ src/GameService.js
@@ -7,6 +7,75 @@ export default class GameService {
     this._stackHeight = stackHeight;
   }
 
+  _getStartCoordinateAntiDiagonal(x, y) {
+    const coord = { x, y };
+    while (coord.x > 0 && coord.y > 0) {
+      coord.x--;
+      coord.y--;
+    }
+    return coord;
+  }
+
+  _getStartCoordinateMainDiagonal(x, y) {
+    const coord = { x, y };
+    while (coord.x > 0 && coord.y < this._stackHeight) {
+      coord.x--;
+      coord.y++;
+    }
+    return coord;
+  }
+
+  _checkAntiDiagonal(stacks, player, x, y) {
+    const coord = this._getStartCoordinateAntiDiagonal(x, y);
+    while (coord.x < this._stackLength && coord.y < this._stackHeight) {
+      let sum = 0;
+
+      for (let i = 0; i < 4; i++) {
+        if (
+          stacks[coord.x + i] &&
+          stacks[coord.x + i][coord.y + i] &&
+          stacks[coord.x + i][coord.y + i] === player
+        ) {
+          sum++;
+        } else {
+          break;
+        }
+      }
+
+      if (sum >= 4) {
+        return true;
+      }
+      coord.x++;
+      coord.y++;
+    }
+    return false;
+  }
+
+  _checkMainDiagonal(stacks, player, x, y) {
+    const coord = this._getStartCoordinateAntiDiagonal(x, y);
+    while (coord.x < this._stackLength && coord.y >= 0) {
+      let sum = 0;
+      for (let i = 0; i < 4; i++) {
+        if (
+          stacks[coord.x + i] &&
+          stacks[coord.x + i][coord.y - i] &&
+          stacks[coord.x + i][coord.y - i] === player
+        ) {
+          sum++;
+        } else {
+          break;
+        }
+      }
+
+      if (sum >= 4) {
+        return true;
+      }
+      coord.x++;
+      coord.y--;
+    }
+    return false;
+  }
+
   _checkHorizontal(stacks, player, y) {
     for (let i = 0; i < this._stackLength - 3; i++) {
       let sum = 0;
@@ -56,7 +125,9 @@ export default class GameService {
   checkWon(stacks, player, x, y) {
     return (
       this._checkHorizontal(stacks, player, y) ||
-      this._checkVertical(stacks, player, x)
+      this._checkVertical(stacks, player, x) ||
+      this._checkMainDiagonal(stacks, player, x, y) ||
+      this._checkAntiDiagonal(stacks, player, x, y)
     );
   }
 }
