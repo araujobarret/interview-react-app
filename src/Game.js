57ff030527e56e2db150239302c6bb212686c380
araujobarret <araujobarret@gmail.com>
2020-02-25 14:42:08 -0300

    Starts with a game service class

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Game extends Component {
-  render() {
-    return (
-      <div className="Game">
-        Connect four game
-      </div>
-    );
-  }
+import GameService from './GameService';
+
+export default function Game() {
+  const gameService = new GameService(7, 6);
+  const [stacks, setStacks] = useState([]);
+
+  useEffect(() => {
+    let emptyBlocks = [];
+    for (let i = 0; i < 6; i++) {
+      emptyBlocks.push(" ");
+    }
+
+    const initialStacks = [];
+    for (let i = 0; i < 7; i++) {
+      initialStacks.push([...emptyBlocks]);
+    }
+    setStacks(initialStacks);
+  }, []);
+  
+  return (
+    <div className="Game">
+      {stacks.map(stack => stack.map(s => <div>{s !== ' ' ? s : '# '}</div>))}
+    </div>
+  );
 }
-
-export default Game;
---
+++ src/GameService.js
@@ -0,0 +1,10 @@
+export default class GameService {
+  _stackHeight;
+  _stackLength;
+
+  constructor(stackLength, stackHeight) {
+    this._stackLength = stackLength;
+    this._stackHeight = stackHeight;
+  }
+
+}
