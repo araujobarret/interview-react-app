8e4b7ef398045b46d4504dde701cf5963de1d113
araujobarret <araujobarret@gmail.com>
2020-02-25 15:27:19 -0300

    Adds validation for row, column and adds some styles to the stack pieces

--- src/Game.js
+++ src/Game.js
@@ -2,26 +2,67 @@ import React, { useState, useEffect } from 'react';
 
 import GameService from './GameService';
 
+const STACK_LENGTH = 7;
+const STACK_HEIGHT = 6;
+
 export default function Game() {
-  const gameService = new GameService(7, 6);
-  const [stacks, setStacks] = useState([]);
+  const gameService = new GameService(STACK_LENGTH, STACK_HEIGHT);
+
+  const emptyBlocks = gameService.getEmptyBlocks();
+  const initialStacks = [];
+  for (let i = 0; i < STACK_LENGTH; i++) {
+    initialStacks.push([...emptyBlocks]);
+  }
+  const [stacks, setStacks] = useState(initialStacks);
+
+  const players = ["red", "blue"];
+  const [currentPlayer, switchCurrentPlayer] = useState(
+    Math.floor(Math.random() * 2)
+  );
+  const [winner, setWinner] = useState(null);
 
   useEffect(() => {
-    let emptyBlocks = [];
-    for (let i = 0; i < 6; i++) {
-      emptyBlocks.push(" ");
+    handleAddPiece(0);
+    handleAddPiece(1);
+    handleAddPiece(0);
+    handleAddPiece(1);
+    handleAddPiece(0);
+    handleAddPiece(1);
+    handleAddPiece(0);
+  }, []);
+
+  const checkWinner = (player, x, y) => {
+    if (gameService.checkWon(stacks, player, x, y)) {
+      setWinner(player);
+    } else {
+      switchCurrentPlayer(currentPlayer === 0 ? 1 : 0);
+    }
+  };
+
+  const handleAddPiece = stackIndex => {
+    if (winner !== null) {
+      return;
     }
 
-    const initialStacks = [];
-    for (let i = 0; i < 7; i++) {
-      initialStacks.push([...emptyBlocks]);
+    const nextStackSlot = stacks[stackIndex].indexOf(" ");
+    const newStacks = stacks;
+    if (nextStackSlot !== -1) {
+      newStacks[stackIndex][nextStackSlot] = players[currentPlayer];
+      setStacks(newStacks);
+      checkWinner(players[currentPlayer], stackIndex, nextStackSlot);
     }
-    setStacks(initialStacks);
-  }, []);
+  };
   
   return (
-    <div className="Game">
-      {stacks.map(stack => stack.map(s => <div>{s !== ' ' ? s : '# '}</div>))}
+    <div>
+      {winner && <div className="message">{`Player ${winner} has won!`}</div>}
+      <div className="board">
+        {stacks.map(stack => (
+          <div className="boardStack">
+          {stack.map(s => <div>{s !== ' ' ? s : '# '}</div>)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/GameService.js
+++ src/GameService.js
@@ -7,4 +7,56 @@ export default class GameService {
     this._stackHeight = stackHeight;
   }
 
+  _checkHorizontal(stacks, player, y) {
+    for (let i = 0; i < this._stackLength - 3; i++) {
+      let sum = 0;
+
+      for (let j = i; j < i + 4; j++) {
+        if (stacks[j][y] === player) {
+          sum++;
+        } else {
+          break;
+        }
+      }
+
+      if (sum >= 4) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  _checkVertical(stacks, player, x) {
+    for (let i = 0; i < this._stackHeight - 3; i++) {
+      let sum = 0;
+
+      for (let j = i; j < i + 4; j++) {
+        if (stacks[x][j] === player) {
+          sum++;
+        } else {
+          break;
+        }
+      }
+
+      if (sum >= 4) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getEmptyBlocks () {
+    const emptyBlocks = [];
+    for (let i = 0; i < this._stackHeight; i++) {
+      emptyBlocks.push(" ");
+    }
+    return emptyBlocks
+  }
+
+  checkWon(stacks, player, x, y) {
+    return (
+      this._checkHorizontal(stacks, player, y) ||
+      this._checkVertical(stacks, player, x)
+    );
+  }
 }
